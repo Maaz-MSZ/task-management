b3e6a388ebf81d230860aab89e765be25e9a8046
Maaz <maaz.shaikh@namlsoft.com>
2025-06-26 17:17:15 +0530

    3rd commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../../task-dashboard/src/features/auth/authSlice';
+import { logout } from '../features/auth/authSlice';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 
